34c6fc51932480541f8cb803eb77b616dd240f8a
abeer <beroghorab068@gmail.com>
2024-03-23 12:58:34 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModal {
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+
+  NoteModal(
+      {required this.color,
+      required this.title,
+      required this.subTitle,
+      required this.date});
+}
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/constants.dart';
 import 'package:notes_app/views/widgets/add_note_buttom_sheet.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
@@ -9,6 +10,8 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
+        foregroundColor: Colors.black,
+        backgroundColor: kPrimaryColor,
         onPressed: () {
           showModalBottomSheet(
               shape: RoundedRectangleBorder(
