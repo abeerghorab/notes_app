83d30885d1da3b76f1ee12749dd4f0ece78afe18
abeer <beroghorab068@gmail.com>
2024-03-27 15:49:51 +0200

    colors list view done

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 //import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
@@ -9,8 +10,10 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+  Color color = Color(0xff5F7470);
 
   addNote(NoteModal note) async {
+    note.color = color.value;
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModal>(kNotesBox);
